41a33c0ab6de557559bd79d351d0fdf4156d6594
SMou <1197864339@qq.com>
2022-03-29 15:54:40 +0800

    0329

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -5,20 +5,29 @@
  * 
  * @LastEditors  : SMou
  * 
- * @LastEditTime : 2022-03-29 15:12:09
+ * @LastEditTime : 2022-03-29 15:23:19
  * 
  * @Description : 请填写简介
  */
 package com.jiawa.wiki.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World Post." + name;
     }
 }
