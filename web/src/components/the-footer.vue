309ce6a1bc3ef8d392c83d98590feead7261e596
SMou <1197864339@qq.com>
2022-04-01 15:35:00 +0800

    增加web界面头和尾，去除helloword组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author       : SMou
+ * @Date         : 2022-04-01 15:24:14
+ * @LastEditors  : SMou
+ * @LastEditTime : 2022-04-01 15:24:30
+ * @Description  : 请填写简介
+-->
+
+<template>
+  <a-layout-footer style="text-align: center"> BOOK </a-layout-footer>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "the-footer",
+});
+</script>
---
+++ web/src/components/the-header.vue
@@ -0,0 +1,31 @@
+<!--
+ * @Author       : SMou
+ * @Date         : 2022-04-01 15:10:18
+ * @LastEditors  : SMou
+ * @LastEditTime : 2022-04-01 15:12:59
+ * @Description  : 请填写简介
+-->
+<template>
+  <a-layout-header class="header">
+    <div class="logo" />
+    <a-menu
+      theme="dark"
+      mode="horizontal"
+      v-model:selectedKeys="selectedKeys1"
+      :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 1</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "the-header",
+});
+</script>
