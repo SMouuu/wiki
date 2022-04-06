22d810b9169afdffa1c6ee995b6289c313216ef9
SMou <1197864339@qq.com>
2022-04-07 01:16:26 +0800

    前端将页面风格改为小写

---
+++ web/src/views/admin/about.vue
@@ -0,0 +1,12 @@
+<!--
+ * @Author       : SMou
+ * @Date         : 2022-04-07 01:09:45
+ * @LastEditors  : SMou
+ * @LastEditTime : 2022-04-07 01:15:06
+ * @Description  : 请填写简介
+-->
+<template>
+  <div class="about">
+    <h1>关于我们</h1>
+  </div>
+</template>
