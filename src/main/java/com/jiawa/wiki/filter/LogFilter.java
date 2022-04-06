80af9936ce895a5f4c6790ef6ab25f0a09aca49c
SMou <1197864339@qq.com>
2022-04-07 00:54:07 +0800

    增加拦截器，打印接口耗时

---
+++ src/main/java/com/jiawa/wiki/filter/LogFilter.java
@@ -0,0 +1,48 @@
+/*
+ * @Author : SMou
+ * 
+ * @Date : 2022-04-01 19:46:26
+ * 
+ * @LastEditors : SMou
+ * 
+ * @LastEditTime : 2022-04-01 19:46:27
+ * 
+ * @Description : 请填写简介
+ */
+package com.jiawa.wiki.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class LogFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException,
+            ServletException {
+        // 打印请求信息
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("------------- LogFilter 开始 -------------");
+        LOG.info("请求地址: {} {}", request.getRequestURL().toString(),
+                request.getMethod());
+        LOG.info("远程地址: {}", request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------",
+                System.currentTimeMillis() - startTime);
+    }
+}
